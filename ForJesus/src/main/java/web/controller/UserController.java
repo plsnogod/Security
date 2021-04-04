5671cf16bd1a706c08131993a3124f31ad06257a
plsnogod <76594493+plsnogod@users.noreply.github.com>
2021-04-04 13:06:41 +0300

    Add files via upload

---
+++ ForJesus/src/main/java/web/controller/UserController.java
@@ -0,0 +1,55 @@
+package web.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+private UserService userService;
+
+    @GetMapping("/")
+    public String showAllUsers(ModelMap model) {
+        model.addAttribute("all_users",userService.showAllUsers());
+        model.addAttribute("user",new User());
+
+
+    return "people/all_users";
+    }
+
+
+    @PostMapping(value = "/all_users")
+    public String addUser(@ModelAttribute User user, Model model){
+        userService.addUser(user);
+        model.addAttribute("users", userService.showAllUsers());
+        model.addAttribute("user", new User());
+        return "people/all_users";
+
+    }
+
+    @PostMapping(value = "/users/update")
+    public String updateUser(@ModelAttribute User user, Model model){
+        userService.updateUser(user);
+        model.addAttribute("users", userService.showAllUsers());
+        return "redirect:/people/all_users";
+
+    }
+
+    @PostMapping(value = "/users/delete")
+    public String deleteUser(@ModelAttribute User user, Model model){
+        userService.deleteUser(user.getId());
+       model.addAttribute("users", userService.showAllUsers());
+        return "redirect:/all_users";
+    }
+
+}
---
+++ ForJesus/src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    @Transactional
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> showAllUsers() {
+        return userDao.showAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(long id) {
+        userDao.deleteUser(id);
+    }
+}
